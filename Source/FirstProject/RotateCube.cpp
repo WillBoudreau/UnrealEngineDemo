b6acaaed3394504944f0e9968ee71970e22aa3a8
WillBoudreau <williamboudreau804@gmail.com>
2025-03-18 14:04:24 -0400

    Figuring out C++

--- Source/FirstProject/RotateCube.cpp
+++ Source/FirstProject/RotateCube.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "RotateCube.h"
+#include "GameFramework/Actor.h"
+#include "Components/ActorComponent.h"
 
 // Sets default values for this component's properties
 URotateCube::URotateCube()
@@ -9,26 +10,23 @@ URotateCube::URotateCube()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	BeginPlay();
 
 	// ...
 }
-
 
 // Called when the game starts
 void URotateCube::BeginPlay()
 {
-	Super::BeginPlay();
+	
 
 	// ...
-	
 }
-
 
 // Called every frame
 void URotateCube::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
 
 	// ...
 }
-
