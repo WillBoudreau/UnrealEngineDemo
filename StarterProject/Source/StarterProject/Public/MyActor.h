fd309f1dcf9f8296bd14101f50668acf5b9e8771
WillBoudreau <williamboudreau804@gmail.com>
2025-03-21 10:54:57 -0400

    Model in the scene
    >Needs to be hooked up

--- StarterProject/Source/StarterProject/Public/MyActor.h
+++ StarterProject/Source/StarterProject/Public/MyActor.h
@@ -22,7 +22,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	UPROPERTY(ViewAnyWhere,Category = "Please");
+	UPROPERTY(ViewAnyWhere,Category = "Please work");
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
 	float PitchValue = 0.0f;
 
