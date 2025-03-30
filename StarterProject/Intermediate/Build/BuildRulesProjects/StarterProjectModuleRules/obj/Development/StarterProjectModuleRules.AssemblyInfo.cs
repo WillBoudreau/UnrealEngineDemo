2f3b08656204447054508e44878f2a333d436a89
WillBoudreau <williamboudreau804@gmail.com>
2025-03-30 19:45:35 -0400

    Area 1 added, intro dialogue too

--- StarterProject/Intermediate/Build/BuildRulesProjects/StarterProjectModuleRules/obj/Development/StarterProjectModuleRules.AssemblyInfo.cs
+++ StarterProject/Intermediate/Build/BuildRulesProjects/StarterProjectModuleRules/obj/Development/StarterProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70791d4864aa6be58384e2cf48abb9adb6bf177b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe78354e712fd9b85950de41b9ae057b1eab51d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StarterProject/Intermediate/Build/Win64/x64/StarterProjectEditor/DebugGame/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/willi/OneDrive/Documents/GitHub/UnrealEngineDemo/FirstProject/StarterProject/Intermediate/Build/Win64/x64/StarterProjectEditor/DebugGame/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
-#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
