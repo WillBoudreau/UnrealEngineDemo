a09a8ceac34d1cb8e1dddb41f8ac50237423b44b
WillBoudreau <williamboudreau804@gmail.com>
2025-04-06 22:00:03 -0400

    Transfered to new project

--- StarterProject/Intermediate/Build/Win64/x64/StarterProject/Shipping/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/willi/OneDrive/Documents/GitHub/UnrealEngineDemo/FirstProject/StarterProject/Intermediate/Build/Win64/x64/StarterProject/Shipping/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
